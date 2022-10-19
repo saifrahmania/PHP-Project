2522c10951c21c232fe422ea17ed20f12bab8ef6
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2022-10-19 18:37:58 +0600

    19/10/2022 6:37 PM

---
+++ day_17/index.php
@@ -0,0 +1,78 @@
+<?php
+
+//$arr = [];
+//$arr['realmi']['model']='f1';
+//$arr['realmi']['price']='8100';
+//$arr['oppo']['model']='redmi';
+//$arr['oppo']['price']='21000';
+//$hhh = array_reverse($arr);
+//echo"<pre>";
+////print_r($arr);
+//print_r($hhh);
+//echo"<pre>";
+?>
+<!--<h1>-->
+<!--    Text Checking-->
+<!--</h1>-->
+<!--<table border="2" cellpadding="5px">-->
+<!--    <tr>-->
+<!--        <th>Model</th>-->
+<!--        <th>Price</th>-->
+<!--    </tr>-->
+<!--    --><?php
+//    foreach($arr as $ar){
+//        echo"
+//        <tr>
+//            <td>{$ar}</td>
+//        </tr>";
+//
+//    }
+//    ?>
+<!---->
+<!--</table>-->
+
+<?php
+//$numbers = [0,1,2,3,4];
+// foreach ($numbers as $num){
+//    echo $num, "<br>";
+// }
+//$aa = array_values($numbers);
+//echo"<pre>";
+////print_r($arr);
+//print_r($aa);
+//echo"<pre>";
+// function functionName(){
+//     //echo"Joy Bangla";
+// }
+
+//functionName();
+//$associative = array("Saiful"=>"teacher","Sazzad"=>"teacher","Saifur"=>"Student","Sabbir"=>"CR");
+//$values = array_keys($associative);
+//echo"<pre>";
+//print_r($values);
+//print_r($associative);
+//for($i=0; $i < count($values); ++$i) {
+    //echo $values[$i] . ' ' . $associative[$values[$i]] . "\n";
+//}
+//echo"<br>";
+//$keys = array_keys($associative);
+//for($i=0; $i < count($keys); ++$i) {
+    //echo $keys[$i] . ' ' . $associative[$keys[$i]] . "\n";
+//}
+
+$myName = "Saifur Rahman";
+$myName = 24;
+//echo $myName;
+$rar=[
+        "f_name"=>[
+        "ff1_name"=>"Muhammad",
+        "ff2_name"=>"Saifur",
+        "middle_name"=>"Rahman",
+        "last_name"=>"Talukder"
+    ],
+    "l_name"=>"Nayeem"];
+//print_r($rar["f_name"]["ff1_name"]);
+print_r($rar["f_name"]["ff1_name"].". ".$rar["f_name"]["ff2_name"]." ".$rar["f_name"]["middle_name"]." ".$rar["f_name"]["last_name"]." ".$rar["l_name"]." ")
+
+
+?>
